c82264272f14d89e4b7ef65bb6eeaf45a8eea2d9
buyaramadhan <ytchochote@gmail.com>
2024-11-22 18:51:07 +0700

    menambahkan objek matakuliah

--- app/src/main/java/com/example/pertemuan7/Data/MataKuliah.kt
+++ app/src/main/java/com/example/pertemuan7/Data/MataKuliah.kt
@@ -1,10 +1,4 @@
 package com.example.pertemuan7.Data
 
 object MataKuliah {
-    val options = listOf (
-        "pemrograman aplikasi mobile lanjut",
-        "pengembangan aplikasi web framework",
-        "prototyping",
-        "multimedia"
-    )
 }
